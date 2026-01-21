1f42c6efd7f223e273214cb5c5d9f4d63573d6e4
高楚鈞 <f256987584@gmail.com>
2026-01-21 11:57:57 +0800

    學習第15單元，嘗試使用 PowerShell ISE跟著練習，並且增加自己的註解說明

---
+++ 15-16_Demo_Install Azure PowerShell From PSGallery/Install_the_Az_PowerShell_module_via_PowershellGet.ps1
@@ -0,0 +1,19 @@
+﻿#### Install the Az PowerShell module via PowerShellGet ####
+############################################################
+
+# There should be atleast RemoteSigned level for Execution Policy
+# MEMO: 確認執行策略，至少要是「RemoteSigned」級別才能夠執行；如果沒有限制就會看到「Unrestricted」
+Get-ExecutionPolicy
+
+# MEMO: 如果要變更執行策略，可以執行下面的指令，選擇想要的執行策略級別(可以用 PowerShell ISE 自動補全的提示查看)
+# 執行策略級別選項：AllSigned、Bypass、Default、RemoteSigned、Restricted、Undefined、Unrestricted
+# 指令範例：
+# Set-ExecutionPolicy -ExecutionPolicy <你要的Execution Policy級別>
+
+# Ensure if PowershellGet Module is installed
+
+# Install Az Module in Current User Scope
+
+# Update Az Module
+
+# Check the Installed Module Versions
